c90c40c82b1b22b5ac8d006895ed9fdf1c33ecf3
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-05-26 20:38:34 -0300

    update

--- script-crud.js
+++ script-crud.js
@@ -2,7 +2,9 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
-const botaoCancelar = document.querySelector('.app__form-footer__button app__form-footer__button--cancel')
+const botaoCancelar = document.querySelector('.app__form-footer__button--cancel')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
+let tarefaSelecionada = null
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
@@ -33,7 +35,6 @@ function criarElementoTarefa(tarefa){
 
     botao.onclick = () => {
         const novaDescricao = prompt('Qual é o nome da tarefa?')
-        console.log('Nova descrição para a tarefa: ',novaDescricao)
         if (novaDescricao){
             paragrafo.textContent = novaDescricao
             tarefa.descricao = novaDescricao
@@ -48,6 +49,21 @@ function criarElementoTarefa(tarefa){
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach (elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            return
+        }
+        tarefaSelecionada = tarefa
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        li.classList.add('app__section-task-list-item-active')
+    }
 
     return li
 }
@@ -72,9 +88,10 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
 
 })
 
-botaoCancelar.addEventListener('click',ocultarFormulario)
-
-
+botaoCancelar.onclick = () => {
+    textArea.value = ''
+    formAdicionarTarefa.classList.toggle('hidden')
+}
 tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
