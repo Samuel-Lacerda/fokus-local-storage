dc38f921a6ad82f8352a18c3a3d50eeb1d97509d
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-07-15 21:27:59 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -23,7 +23,7 @@ function criarElementoTarefa(tarefa){
 
     const svg = document.createElement('svg')
     svg.innerHTML = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+    <circle class="circle-status" cx="12" cy="12" r="12" fill="#FFF"></circle>
     <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
 </svg>`
 
@@ -66,6 +66,14 @@ function criarElementoTarefa(tarefa){
         liTarefaSelecionada = li
         paragrafoDescricaoTarefa.textContent = tarefa.descricao
         li.classList.add('app__section-task-list-item-active')
+    }
+
+    const circle = li.querySelector('.circle-status')
+    circle.onclick = () => {
+        li.classList.remove('app__section-task-list-item-active')
+        li.classList.add('app__section-task-list-item-complete')
+        botao.onclick = () =>{}
+        li.onclick = () => {}
     }
 
     return li
